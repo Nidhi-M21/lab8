bee8907616fc4ede444d001c17864fe0bf6c51f4
Nidhi Mogatlal <nidhimogatlal@gmail.com>
2025-11-22 19:25:15 +0000

    page tecnologias feita

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -4,27 +4,32 @@ import Image from 'next/image'
 export default function page() {
     const listaTecnologias = JSON.parse(JSON.stringify(tecnologias))
     return (
-        <section className="flex justify-center items-center flex-col">
-               <h2 className="text-2xl font-bold mb-6">Tecnologias Exploradas</h2>
+        <main className="w-[600px] mx-auto">
+            <h2 className="text-2xl font-bold  text-center">Tecnologias Exploradas</h2>
+            <section className="grid grid-cols-3 gap-1 m-2">
+                {listaTecnologias.map((tec, i) => (
+                    <article 
+                    key={i}
+                     className='flex justify-center items-center flex-col m-3 bg-blue-500 rounded-xl shadow'>
+                        <h2>{tec.title}</h2>
+                        <Image
+                            src={`/tecnologias/${tec.image}`}
+                            alt={`Logotipo do ${tec.title}`}
+                            width={100}
+                            height={100}
+                            className="mb-4 m-4"
+                        />
+                        <p className='p-2'>{tec.description}</p>
+                        <p>{tec.rating}</p>
+                    </article>
 
-            {listaTecnologias.map((tec, i) => (
-                <article className='flex justify-center items-center flex-col m-3 bg-blue-500 rounded-xl shadow'>
-                    <h2>{tec.title}</h2>
-                    <Image
-                        src={`/tecnologias/${tec.image}`}
-                        alt={`Logotipo do ${tec.title}`}
-                        width={100}
-                        height={100}
-                        className="mb-4 m-4"
-                    />
-                    <p>{tec.description}</p>
-                    <p>{tec.rating}</p>
-                </article>
+                ))}
 
-            ))}
+            </section>
 
-        </section>
-         
-        
+        </main>
+
+
+
     )
 }
